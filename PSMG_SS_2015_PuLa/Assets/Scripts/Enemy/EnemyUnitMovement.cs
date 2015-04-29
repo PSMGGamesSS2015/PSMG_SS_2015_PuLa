145883d1a8176b103bd3c6e4a3d439385a05366c
Hans-Martin Schuller <hm.schuller@googlemail.com>
2015-04-29 20:31:47 +0200

    started Enemy Scripting

---
+++ PSMG_SS_2015_PuLa/Assets/Scripts/Enemy/EnemyUnitMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUnitMovement : MonoBehaviour {
+	/**
+	 * Script for the basic movement of the enemy unit.
+	 * Enemy unit will rest on position ( for now, in future a patrolling feature could be added )
+	 * once it spots the player ( if sphere collider triggers ) it will chase the player until either one dies
+	 * Enemy can be killed by shooting ( with the lama )
+	 **/
+	// Use this for initialization
+	private Rigidbody rBody;
+	private bool isAlive;
+	private bool isChasing;
+	private float movePower;
+	private float damageOnHit;
+
+	void Start () {
+		rBody = GetComponent<Rigidbody> ();
+		isAlive = true;
+		isChasing = false;
+		movePower = 5f;
+		damageOnHit = 1;
+	}
+	
+	void FixedUpdate () {
+		if(isAlive){
+			doUpdate();
+		}
+	}
+
+	void doUpdate(){
+
+	}
+}
