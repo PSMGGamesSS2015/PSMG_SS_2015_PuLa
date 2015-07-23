b1511390ad5b7175c3cad0bcf3898735de3539f8
Julifunk <julia@groetsch.org>
2015-07-23 23:35:59 +0200

    Start Screen
    Added Start Screen to the Build. Right now only the "Start" Icon has
    function, rest coming soon.

---
+++ PSMG_SS_2015_PuLa/Assets/Scripts/UI/StartScreenScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartScreenScript : MonoBehaviour {
+
+	public Button start;
+	public Button settings;
+	public Button lIcon;
+	public Button pIcon;
+
+	// Use this for initialization
+	void Start () {
+		start = start.GetComponent<Button> ();
+		settings = settings.GetComponent<Button> ();
+		lIcon = lIcon.GetComponent<Button> ();
+		pIcon = pIcon.GetComponent<Button> ();
+	}
+
+	public void startPress(){
+		Application.LoadLevel (1);
+
+	}
+	public void settingsPress(){
+	}
+
+	public void ludwigStartMenuPress(){
+	}
+
+	public void pabloStartMenuPress(){
+	}
+}
