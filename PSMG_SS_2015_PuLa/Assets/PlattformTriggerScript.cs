ff72565fcf22e431c42838d6c4c19bc994421093
Hans Schuller <hm.schuller@googlemail.com>
2015-08-27 20:31:55 +0200

    plattform challenge puma

---
+++ PSMG_SS_2015_PuLa/Assets/PlattformTriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlattformTriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Plattform") {
+			Debug.Log ("Trigger");
+			collider.GetComponent<PlattformMovementScript>().movingDirection *= -1;
+		}
+	}
+}
---
+++ PSMG_SS_2015_PuLa/Assets/RespawnPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnPlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	private Transform spawnAt;
+	void Start () {
+		spawnAt = transform.Find ("RespawnPositionPuma");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Player") {
+			// Damage Player start
+
+			// Damage Player end 
+			collider.transform.position = spawnAt.position;
+		}
+	}
+}
