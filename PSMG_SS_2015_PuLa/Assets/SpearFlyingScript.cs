d7610f7fde909b5c85c6c794f6b5a3e9635728ba
Hans Schuller <hm.schuller@googlemail.com>
2015-08-27 19:03:18 +0200

    first Challenge Puma
    Spears Challenge complete

---
+++ PSMG_SS_2015_PuLa/Assets/SpearFlyingScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearFlyingScript : MonoBehaviour {
+
+	private Rigidbody rBody;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Shoot(Transform target){
+		rBody = GetComponent<Rigidbody> ();
+		transform.LookAt (target);
+		Vector3 velocity = (target.position - transform.position).normalized * 10;
+		rBody.velocity = velocity;
+		rBody.AddForce (Vector3.up * -10);	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Finish") {
+			Destroy(this.gameObject);
+		}
+		if (collider.tag == "Player") {
+			// Damage Player start
+
+			// Damage Player end
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ PSMG_SS_2015_PuLa/Assets/SpearShootingScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearShootingScript : MonoBehaviour {
+	
+	// Use this for initialization
+	public GameObject spearObject;
+	private Transform flyTo;
+	private Transform flyOrigin;
+	void Start () {
+		flyTo = transform.Find ("FlyTo").transform;
+		flyOrigin = transform.Find ("FlyOrigin").transform;
+		StartCoroutine (WaitAndShoot ());
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	void Shoot() {
+		StartCoroutine (WaitAndShoot ());
+	}
+
+	IEnumerator WaitAndShoot() {
+		SpawnSpear ();
+		yield return new WaitForSeconds(Random.Range(1,5));
+		Shoot ();
+	}
+
+	private void SpawnSpear() {
+		GameObject spear1 = (GameObject) Instantiate(spearObject, flyOrigin.position, Quaternion.identity);
+		spear1.GetComponent<SpearFlyingScript>().Shoot (flyTo);
+	}
+}
