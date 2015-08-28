b689eb16a188784519c5f49048e5a84731fb45bf
schmargi <maike@schmargi.de>
2015-08-28 17:36:14 +0200

    try to implement the animation

---
+++ PSMG_SS_2015_PuLa/Assets/Prefab/animControllerLudwig.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animControllerLudwig : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButton ("Space")) {
+			anim.SetBool ("walkToJump", true);
+		} else {
+			anim.SetBool ("walkToJump", false);
+
+		}
+		if (Input.GetButton ("x")) {
+			anim.SetBool ("walkToClout", true);
+		} else { 
+			anim.SetBool ("walkToClout", false);	
+		}
+	}
+}
