7fb297ead8371e50e10f5abf67b16d15a9f23bb6
Hans Schuller <hm.schuller@googlemail.com>
2015-08-29 13:41:54 +0200

    Implementation of Lama Challenge started
    Color sequence showcase implemented

---
+++ PSMG_SS_2015_PuLa/Assets/ColorChallengeScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChallengeScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] ColorTiles;
+	public Material HighlightColor;
+
+	public enum TileColor { Red, Blue, Yellow, Orange, Green };
+	
+	void Start () {
+		TileColor[] test = {TileColor.Blue, TileColor.Yellow, TileColor.Red, TileColor.Orange, TileColor.Yellow, TileColor.Green};
+		StartCoroutine (highLightTileForShow (test));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator highLightTileForShow(TileColor[] sequence) {
+		foreach (TileColor currentTileIndex in sequence) {
+			StartCoroutine(changeColorToHighlightAndBack(ColorTiles[(int)currentTileIndex]));
+			yield return new WaitForSeconds(2);
+		}
+	}
+
+	IEnumerator changeColorToHighlightAndBack(GameObject tileObject) {
+		Debug.Log (getTile (tileObject));
+		Material originalColorOfTile = getTile(tileObject).GetComponent<Renderer> ().material;
+		changeColorOfTile (tileObject, HighlightColor);
+		yield return new WaitForSeconds (1);
+		changeColorOfTile (tileObject, originalColorOfTile); 
+	}
+		
+	public void changeColorOfTile(GameObject tileObject, Material materialOfColor) {
+		getTile(tileObject).GetComponent<Renderer> ().material = materialOfColor;
+	}
+
+	private GameObject getTile(GameObject tileObject){
+		return tileObject.transform.FindChild ("Plane").gameObject;
+	}
+}
