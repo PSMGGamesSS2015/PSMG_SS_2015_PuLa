f6ae93e747e69789ea0e3babe647ece5d7bb2d1e
Hans Schuller <hm.schuller@googlemail.com>
2015-08-31 18:57:12 +0200

    scripting of level almost done!!!

--- PSMG_SS_2015_PuLa/Assets/DoorEnemiesScript.cs
+++ PSMG_SS_2015_PuLa/Assets/DoorEnemiesScript.cs
@@ -8,6 +8,10 @@ public class DoorEnemiesScript : MonoBehaviour {
 	public GameObject door2;
 	public Transform lookFrom;
 	public Transform lookTo;
+
+	public GameObject[] enemies;
+
+	private bool isReady = true;
 	void Start () {
 	
 	}
@@ -18,11 +22,21 @@ public class DoorEnemiesScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider collider) {
-		if (collider.tag == "Player") {
-			door1.GetComponent<MoveScript>().activate ();
-			door2.GetComponent<MoveScript>().activate ();
-			GameObject.Find ("Main Camera").GetComponent<SmoothThirdPersonCamera>().CameraEvent(lookFrom, lookTo, 2);
-			collider.gameObject.GetComponent<PlayerSwap>().doSwap();
+		if (isReady) {
+			if (collider.tag == "Player") {
+				door1.GetComponent<MoveScript> ().activate ();
+				door2.GetComponent<MoveScript> ().activate ();
+				GameObject.Find ("Main Camera").GetComponent<SmoothThirdPersonCamera> ().CameraEvent (lookFrom, lookTo, 2);
+				collider.gameObject.GetComponent<PlayerSwap> ().doSwap ();
+				isReady = false;
+				activateEnemies();
+			}
+		}
+	}
+
+	private void activateEnemies() {
+		foreach (GameObject enemy in enemies) {
+			enemy.GetComponent<Level3EnemyMovementScript>().activate();
 		}
 	}
 }
